395dda209355f9c60b98cce7237fbf0b0e577852
FrCln <54150197+FrCln@users.noreply.github.com>
2020-10-13 20:00:39 +0300

    added multiline selection processing

--- sublime_trimmer.py
+++ sublime_trimmer.py
@@ -5,14 +5,17 @@ from .utils import trim_line
 
 
 class TrimLineCommand(sublime_plugin.TextCommand):
+    ''' Trim and format line '''
     def run(self, edit):
 
         pos = self.view.sel()[0].begin()
         lines = self.view.lines(self.view.sel()[0])
+        region = sublime.Region(lines[0].begin(), lines[-1].end())
 
-        for line in lines:
-            self.view.replace(
-                edit,
-                line,
-                str(trim_line(self.view.substr(line)))
-            )
+        result = '\n'.join(trim_line(self.view.substr(line)) for line in lines)
+
+        self.view.replace(
+            edit,
+            region,
+            str(result)
+        )
